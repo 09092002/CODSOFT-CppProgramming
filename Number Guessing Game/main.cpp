6d944d22b129e90501724d421ad256c93da519dc
Harsh Tiwari <83784179+09092002@users.noreply.github.com>
2023-07-31 22:33:12 +0530

    Add files via upload

---
+++ Number Guessing Game/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int num, guess, tries = 0; 
+	srand(time(0)); //seed random number generator
+	num = rand() % 10 + 1; // random number between 1 and 100
+	cout << "Hello, my name is Harsh Tiwari, and you are participating in Number Guessing Game \nLet's Get Started\n\n";
+
+	do
+	{
+		cout << "Enter a guess between 1 and 10 : ";
+		cin >> guess;
+		tries++;
+
+		if (guess > num)
+			cout << "Too high!\n\n";
+		else if (guess < num)
+			cout << "Too low!\n\n";
+		else
+			cout << "\nCorrect! You got it in " << tries << " guesses!\n";
+	} while (guess != num);
+
+	return 0;
+}
